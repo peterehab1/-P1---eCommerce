eb832f6f63836ec8268576c8ceffd7f63279b63e
peterehab01 <petereroshdy@gmail.com>
2017-12-14 17:28:15 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+include "init/init.php";
+include "init/includes/header.php";
+
+?>
+
+<h1 class="starter">Hello, Peter</h1>
+
+
+<?php
+include "init/includes/footer.php";
+?>
---
+++ init/includes/footer.php
@@ -0,0 +1,7 @@
+    <!-- jQuery first, then Popper.js, then Bootstrap JS, Other -->
+    <script src="assets/js/jquery-3.2.1.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+  </body>
+</html>
